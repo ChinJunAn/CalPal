66ba1ffb92a14a8c00a7298a28791a030145bdf4
ChinJunAn <junan1599@gmail.com>
2023-10-21 15:46:20 +0800

    Designed web and implemented buttons

--- Server/WebServer.py
+++ Server/WebServer.py
@@ -1,19 +1,85 @@
 from flask import Flask, request, render_template
-from Classifier import classifyItem, calculateCal
+from Classifier import caloriesInFunc
 from MQTT import connectMQTT
 import os
 import threading
+import Variables
 # Create the Flask object
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-image_dir = "Picture"
-image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".ico"]
+item = "Place food on smart plate"
+weight = ""
+calories_in = "Then click on the \"Calories-In\" button"
+activity = "Go for a run with the activity tracker"
+duration = ""
+calories_out = "Then click on the \"Calories-Out\" button"
+net_calories = "Get both Calories-In and Calories-Out first!!"
 
+template_data = {
+    "food": item,
+    "weight": weight,
+    "caloriesIn": calories_in,
+    "activity": activity,
+    "duration": duration,
+    "caloriesOut": calories_out,
+    "netCalories": net_calories
+}
+def updateTemplateData(item, weight, calories_in, activity, duration, calories_out, net_calories):
+    global template_data
+    template_data = {
+        "food": item,
+        "weight": weight,
+        "caloriesIn": calories_in,
+        "activity": activity,
+        "duration": duration,
+        "caloriesOut": calories_out,
+        "netCalories": net_calories
+    }
 
-@app.route('/', methods = ['GET'])
+calories_in_flag = False
+calories_out_flag = False
+def updateNetCalories():
+    global net_calories, calories_in_flag, calories_out_flag
+    if calories_in_flag and calories_out_flag:
+        global net_calories
+        net_calories = str(float(calories_in) - float(calories_out))
+
+@app.route('/')
 def root():
-    return 'CS3237 Sample Site', 200
+    return render_template('index.html', info = template_data), 200
 
+@app.route('/Calories_In')
+def caloriesIn():
+    try:    
+        results = caloriesInFunc(Variables.image_dir)
+        if results is not None:
+            global item, weight, calories_in, calories_in_flag
+            item, weight, calories_in = results
+            calories_in_flag = True
+        updateNetCalories()
+        updateTemplateData(item, weight, calories_in, activity, duration, calories_out, net_calories)
+        return render_template('index.html', info = template_data), 200
+    except Exception as e:
+        return render_template('index.html', info = {"title":"No item found ", "name":e}), 200
+
+@app.route('/Calories_Out')
+def caloriesOut():
+    try:
+        #activty, duration, calories_out = caloriesOutFunc()
+        return render_template('index.html', info = template_data), 200
+    except Exception as e:
+        return render_template('index.html', info = {"title":"No item found ", "name":e}), 200
+
+def main():
+    global client, db, col, app
+    thread = threading.Thread(target=connectMQTT)
+    thread.start()
+    app.run(port = 3237)
+
+if __name__ == '__main__':
+    main()
+
+'''
 # Examples of how to render a template. Also note
 # how we use requests.args.get to extract GET parameters
 @app.route('/index', methods = ['GET'])
@@ -27,37 +93,4 @@ def index():
         name = 'Bob Jones'
 
     return render_template('index.html', info = {"title":"Hello World", "name":name}), 200
-
-@app.route('/classify')
-def identify():
-    try:
-        # check if image have been received and saved
-        files = os.listdir(image_dir)
-        image_found = False
-        image_file = ""
-        for file in files:
-            if os.path.isfile(os.path.join(image_dir, file)):
-                file_extension = os.path.splitext(file)[1].lower()  # Get the file extension in lowercase
-                if file_extension in image_extensions:
-                    image_found = True
-                    image_file = os.path.join(image_dir, file)
-                    break
-        if image_found:
-            item = classifyItem(image_file)
-            calories = calculateCal(image_file, item)
-            return render_template('index.html', info = {"title":"Item: ", "name":str(item)+" "+calories}), 200
-        else:
-            return render_template('index.html', info = {"title":"No item found", "name":" no item found"}), 200
-        
-    except Exception as e:
-        return render_template('index.html', info = {"title":"No item found ", "name":e}), 200
-
-def main():
-    global client, db, col, app
-    thread = threading.Thread(target=connectMQTT)
-    thread.start()
-    app.run(port = 3237)
-
-if __name__ == '__main__':
-    main()
-
+'''
